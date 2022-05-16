80b70e127aa618baaaa691a6880b940b41798201
Amogh Rijal <amoghrijal@Amoghs-MacBook.local>
2022-05-16 14:41:19 +0545

    Added native fzf and other minor changes

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -7,9 +7,7 @@ if fn.empty(fn.glob(install_path)) > 0 then
     "git", "clone",
     "--depth",
     "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
+    "https://github.com/wbthomason/packer.nvim", install_path, }
   print "Installing packer close and reopen Neovim..."
   vim.cmd [[packadd packer.nvim]]
 end
@@ -79,6 +77,7 @@ return packer.startup(function(use)
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 
   -- Treesitter
   use {
